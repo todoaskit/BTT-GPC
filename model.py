1f58d0beea206206d97f829b897cad832666f4de
Dongkwan Kim <todoaskit@gmail.com>
2018-09-25 10:04:32 +0900

    Modularize GPC

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import List, Dict
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,20 +17,43 @@ http://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpc.html
 http://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpc_iris.html
 """
 
+
+class GPC:
+
+    def __init__(self, kernel_list, n_splits=5):
+        # Data
+        self.loader = DataLoader('{}.txt'.format(FILE_NAME))
+
+        # Model
+        self.gpc_dict: Dict[int, List[GaussianProcessClassifier]] = defaultdict(list)
+        for k in range(n_splits):
+            for kernel in kernel_list:
+                self.gpc_dict[k].append(GaussianProcessClassifier(kernel=kernel))
+
+        print('initialized with {} kernels, {} n_splits'.format(len(kernel_list), n_splits))
+
+    def fit(self, fold):
+        X_train, y_train, X_test, y_test = self.loader.get_train_test_xy(fold)
+        for gpc in self.gpc_dict[fold]:
+            gpc.fit(X_train, y_train)
+            print('fit: {}'.format(gpc.kernel))
+
+    def eval(self, fold):
+        X_train, y_train, X_test, y_test = self.loader.get_train_test_xy(fold)
+        for gpc in self.gpc_dict[fold]:
+            train_acc = accuracy_score(y_train, gpc.predict(X_train))
+            test_acc = accuracy_score(y_test, gpc.predict(X_test))
+            print("="*10)
+            print("Kernel: {}".format(gpc.kernel))
+            print("Train Acc: {}".format(train_acc))
+            print("Test Acc: {}".format(test_acc))
+
+
 if __name__ == '__main__':
-
-    # Data
-    FOLD = 0
-    loader = DataLoader('{}.txt'.format(FILE_NAME))
-    X_train, y_train, X_test, y_test = loader.get_train_test_xy(fold=FOLD)
-
-    # Model
-    kernel = 1.0 * RBF([1.0])
-    gpc = GaussianProcessClassifier(kernel=kernel)
-    gpc.fit(X_train, y_train)
-
-    train_acc = accuracy_score(y_train, gpc.predict(X_train))
-    test_acc = accuracy_score(y_test, gpc.predict(X_test))
-
-    print("Train Accuracy: {}".format(train_acc))
-    print("Test Accuracy: {}".format(test_acc))
+    gp_classifier = GPC(
+        kernel_list=[
+            1.0 * RBF([1.0]),
+        ],
+    )
+    gp_classifier.fit(fold=0)
+    gp_classifier.eval(fold=0)
