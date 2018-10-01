8e6e470e7966051f04c796b21ba43cb960438e9d
Dongkwan Kim <todoaskit@gmail.com>
2018-10-01 18:08:56 +0900

    Testing various kernels

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.metrics.classification import accuracy_score, log_loss
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, RationalQuadratic, ExpSineSquared, ConstantKernel, DotProduct, Matern
 from sklearn import decomposition
 from sklearn import manifold
 
@@ -15,10 +15,13 @@ from dataset import DataLoader, FILE_NAME
 from useful import timeit
 
 """
-Reference
+References for GPC
 http://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessClassifier.html
 http://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpc.html
 http://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpc_iris.html
+
+For kernels,
+http://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.kernels.Matern.html
 """
 
 
@@ -52,7 +55,7 @@ class GPC:
         print("Fold: {}, Kernel: {}".format(fold, gpc.kernel))
         print("Train Acc: {}".format(train_acc))
         print("Test Acc: {}".format(test_acc))
-        print("="*10)
+        print("=" * 10)
 
     def run(self, fold):
         self.fit(fold)
@@ -70,9 +73,21 @@ if __name__ == '__main__':
     data_loader = DataLoader('{}.txt'.format(FILE_NAME))
     data_loader.transform_x(manifold.TSNE, n_components=3)
 
-    now_kernel = random() * RBF([randrange(1, 10)])
-    gp_classifier = GPC(
-        kernel=now_kernel,
-        loader=data_loader,
-    )
-    gp_classifier.run(fold=now_fold)
+    kernels = [
+        1.0 * Matern(length_scale=random() * randrange(1, 5),
+                     length_scale_bounds=(1e-5, 1e5),
+                     nu=2.5)
+        + 1.0 * Matern(length_scale=random() * randrange(1, 5),
+                       length_scale_bounds=(1e-5, 1e5),
+                       nu=1.5)
+    ]
+
+    for now_kernel in kernels:
+        try:
+            gp_classifier = GPC(
+                kernel=now_kernel,
+                loader=data_loader,
+            )
+            gp_classifier.run(fold=now_fold)
+        except Exception as e:
+            print(e)
